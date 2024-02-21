b5da821bf0aeae9e07afdb812e77a9a386c29e61
Arturas Mickiewicz <57557793+arturasmckwcz@users.noreply.github.com>
2024-02-21 20:10:52 +0100

    updated nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,42 @@
 <template>
-  <header
-    class="sticky top-0 bg-white shadow-md h-16 flex justify-between items-center px-4"
-  >
-    <router-link
-      :to="{ name: 'home' }"
-      class="px-2 transition-colors hover:text-blue-600"
-    >
-      <img width="16" src="../assets/home.256x255.png" alt="Home" />
-    </router-link>
-    <div class="flex gap-5">
-      <router-link
-        :to="{ name: 'byLetter' }"
-        class="px-2 h-full transition-colors hover:text-blue-600"
-        >Meals By Letter</router-link
-      >
-      <router-link
-        :to="{ name: 'ingredients' }"
-        class="px-2 h-full transition-colors hover:text-blue-600"
-        >Ingredients</router-link
-      >
+  <header class="sticky top-0 bg-gray-100">
+    <div>
+      <img src="../assets/the-meal-db-logo-small.png" alt="meal-db-logo" />
     </div>
+    <nav class="bg-white shadow-md h-16 flex justify-between items-center px-4">
+      <router-link
+        :to="{ name: 'home' }"
+        class="px-2 transition-colors hover:text-blue-600"
+      >
+        <img width="16" src="../assets/home.256x255.png" alt="Home" />
+      </router-link>
+      <div class="flex gap-5">
+        <router-link
+          :to="{ name: 'areas' }"
+          class="px-2 h-full transition-colors hover:text-blue-600"
+        >
+          Areas
+        </router-link>
+        <router-link
+          :to="{ name: 'categories' }"
+          class="px-2 h-full transition-colors hover:text-blue-600"
+        >
+          Categories
+        </router-link>
+        <router-link
+          :to="{ name: 'ingredients' }"
+          class="px-2 h-full transition-colors hover:text-blue-600"
+        >
+          Ingredients
+        </router-link>
+        <router-link
+          :to="{ name: 'byLetter' }"
+          class="px-2 h-full transition-colors hover:text-blue-600"
+        >
+          Meals By Letter
+        </router-link>
+      </div>
+    </nav>
   </header>
 </template>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,51 @@
 import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
 
 import DefaultLayout from "../components/DefaultLayout.vue";
-import Home from "../views/Home.vue";
 import MealsByKeyword from "../views/MealsByKeyword.vue";
 import MealsByLetter from "../views/MealsByLetter.vue";
-import MealsByIngredient from "../views/MealsByIngredient.vue";
 import MealDetails from "../views/MealDetails.vue";
 import GuestLayout from "../components/GuestLayout.vue";
+import Areas from "../views/Areas.vue";
+import MealsByArea from "../views/MealsByArea.vue";
+import Categories from "../views/Categories.vue";
+import MealsByCategory from "../views/MealsByCategory.vue";
 import Ingredients from "../views/Ingredients.vue";
+import MealsByIngredient from "../views/MealsByIngredient.vue";
 
 const routes: RouteRecordRaw[] = [
   {
     path: "/",
     component: DefaultLayout,
     children: [
-      // {
-      //   path: "/",
-      //   name: "home",
-      //   component: Home,
-      // },
       {
         path: "/",
         name: "home",
         component: MealsByKeyword,
       },
       {
-        path: "/by-letter/:letter?",
-        name: "byLetter",
-        component: MealsByLetter,
+        path: "/meal/:id?",
+        name: "mealDetails",
+        component: MealDetails,
+      },
+      {
+        path: "/areas",
+        name: "areas",
+        component: Areas,
+      },
+      {
+        path: "/by-area/:area?",
+        name: "byArea",
+        component: MealsByArea,
+      },
+      {
+        path: "/categories",
+        name: "categories",
+        component: Categories,
+      },
+      {
+        path: "/by-category/:category?",
+        name: "byCategory",
+        component: MealsByCategory,
       },
       {
         path: "/ingredients",
@@ -40,9 +58,9 @@ const routes: RouteRecordRaw[] = [
         component: MealsByIngredient,
       },
       {
-        path: "/meal/:id?",
-        name: "mealDetails",
-        component: MealDetails,
+        path: "/by-letter/:letter?",
+        name: "byLetter",
+        component: MealsByLetter,
       },
     ],
   },
