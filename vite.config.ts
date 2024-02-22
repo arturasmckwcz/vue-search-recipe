49e651b4e6fd05442da2a1739c0e0e7912119206
Arturas Mickiewicz <57557793+arturasmckwcz@users.noreply.github.com>
2024-02-22 19:04:33 +0100

    now default is /

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import "dotenv/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_FE_BASE || "/main",
+  base: process.env.VITE_FE_BASE || "/",
   plugins: [vue()],
 });
