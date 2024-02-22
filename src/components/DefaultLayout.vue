3d27bb10fadc2c670b0b659edb6f4b809fa139a8
Arturas Mickiewicz <57557793+arturasmckwcz@users.noreply.github.com>
2024-02-22 21:57:49 +0100

    added container for max width

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -1,6 +1,8 @@
 <template>
-  <navbar />
-  <main class="min-h-full"><router-view /></main>
+  <div class="max-w-[100ch] mx-auto">
+    <navbar />
+    <main class="min-h-full"><router-view /></main>
+  </div>
 </template>
 
 <script setup lang="ts">
