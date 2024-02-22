7f128d31a5900635cd2bedf31012467ed72e8f30
Arturas Mickiewicz <57557793+arturasmckwcz@users.noreply.github.com>
2024-02-22 18:36:17 +0100

    fixed as meta is not available

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_FE_BASE_URL,
+  base: process.env.VITE_FE_BASE || "/main",
   plugins: [vue()],
 });
