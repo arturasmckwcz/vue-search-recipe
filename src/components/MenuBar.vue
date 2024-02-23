13989ecd3ae50903b8254a4752b87f49a0000233
Arturas Mickiewicz <57557793+arturasmckwcz@users.noreply.github.com>
2024-02-23 23:14:46 +0100

    got navbar responsive

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,30 +1,28 @@
 <template>
   <div>
-    <router-link
-      :to="{ name: 'areas' }"
-      class="px-2 h-full transition-colors hover:text-blue-600"
-    >
-      Areas
-    </router-link>
-    <router-link
-      :to="{ name: 'categories' }"
-      class="px-2 h-full transition-colors hover:text-blue-600"
-    >
+    <router-link :to="{ name: 'areas' }" :class="strClass"> Areas </router-link>
+    <router-link :to="{ name: 'categories' }" :class="strClass">
       Categories
     </router-link>
-    <router-link
-      :to="{ name: 'ingredients' }"
-      class="px-2 h-full transition-colors hover:text-blue-600"
-    >
+    <router-link :to="{ name: 'ingredients' }" :class="strClass">
       Ingredients
     </router-link>
-    <router-link
-      :to="{ name: 'byLetter' }"
-      class="px-2 h-full transition-colors hover:text-blue-600"
-    >
+    <router-link :to="{ name: 'byLetter' }" :class="strClass">
       Meals By Letter
     </router-link>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+
+const { childClass } = defineProps({
+  childClass: {
+    type: String,
+    required: false,
+  },
+});
+const strClass = computed(
+  () => childClass + " px-2 h-full transition-colors hover:text-blue-600"
+);
+</script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,11 +14,31 @@
       >
         <img width="16" src="../assets/home.256x255.png" alt="Home" />
       </router-link>
-      <MenuBar class="sm:flex sm:gap-5 sm:flex-col" />
+      <div class="SANDWICH sm:hidden grid place-items-center relative">
+        <span @click="showMenuBar = !showMenuBar">
+          <img width="16" src="../assets/menu_2976215.png" alt="" />
+        </span>
+        <MenuBar
+          v-if="showMenuBar"
+          class="flex flex-col w-[13ch] absolute top-6 right-0"
+          childClass="border bg-white shadow-lg text-right"
+          @click="showMenuBar = false"
+        />
+      </div>
+      <MenuBar class="hidden sm:flex sm:gap-5" />
     </nav>
   </header>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import MenuBar from "./MenuBar.vue";
+import { onMounted } from "vue";
+
+const showMenuBar = ref(false);
+
+onMounted(() => {
+  console.log("DEBUG:onMounted:showMenuBar:", showMenuBar.value);
+  window.addEventListener;
+});
 </script>
