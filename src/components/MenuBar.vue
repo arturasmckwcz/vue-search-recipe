e6cf3d2c32de4f79449b98539044452e831d3a2c
Arturas Mickiewicz <57557793+arturasmckwcz@users.noreply.github.com>
2024-02-23 16:51:02 +0100

    moved menu items to component

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <router-link
+      :to="{ name: 'areas' }"
+      class="px-2 h-full transition-colors hover:text-blue-600"
+    >
+      Areas
+    </router-link>
+    <router-link
+      :to="{ name: 'categories' }"
+      class="px-2 h-full transition-colors hover:text-blue-600"
+    >
+      Categories
+    </router-link>
+    <router-link
+      :to="{ name: 'ingredients' }"
+      class="px-2 h-full transition-colors hover:text-blue-600"
+    >
+      Ingredients
+    </router-link>
+    <router-link
+      :to="{ name: 'byLetter' }"
+      class="px-2 h-full transition-colors hover:text-blue-600"
+    >
+      Meals By Letter
+    </router-link>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,42 +6,19 @@
       </a>
     </div>
     <nav
-      class="bg-white shadow-md h-16 rounded flex justify-between items-center px-4"
+      class="bg-white shadow-md h-16 rounded flex justify-between items-center px-6"
     >
       <router-link
         :to="{ name: 'home' }"
-        class="px-2 transition-colors hover:text-blue-600"
+        class="transition-colors hover:text-blue-600"
       >
         <img width="16" src="../assets/home.256x255.png" alt="Home" />
       </router-link>
-      <div class="flex gap-5">
-        <router-link
-          :to="{ name: 'areas' }"
-          class="px-2 h-full transition-colors hover:text-blue-600"
-        >
-          Areas
-        </router-link>
-        <router-link
-          :to="{ name: 'categories' }"
-          class="px-2 h-full transition-colors hover:text-blue-600"
-        >
-          Categories
-        </router-link>
-        <router-link
-          :to="{ name: 'ingredients' }"
-          class="px-2 h-full transition-colors hover:text-blue-600"
-        >
-          Ingredients
-        </router-link>
-        <router-link
-          :to="{ name: 'byLetter' }"
-          class="px-2 h-full transition-colors hover:text-blue-600"
-        >
-          Meals By Letter
-        </router-link>
-      </div>
+      <MenuBar class="sm:flex sm:gap-5 sm:flex-col" />
     </nav>
   </header>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import MenuBar from "./MenuBar.vue";
+</script>
